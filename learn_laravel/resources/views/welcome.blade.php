b11db78773f47a54e4944eea6d8dfea91d248cbd
Martin <koudela.m@icloud.com>
2025-10-27 22:17:48 +0100

    initialized postgre DB, did welcome menu + routes

--- learn_laravel/resources/views/welcome.blade.php
+++ learn_laravel/resources/views/welcome.blade.php
@@ -5,11 +5,39 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Home</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
-<body>
 
-<h1>Hello from Laravel</h1>
-
+<body class="bg-gray-900">
+<div class="bg-gray-900">
+    <div class="mx-auto max-w-7xl py-24 sm:px-6 sm:py-32 lg:px-8">
+        <div
+            class="relative isolate overflow-hidden bg-gray-800 px-6 pt-16 after:pointer-events-none after:absolute after:inset-0 after:inset-ring after:inset-ring-white/10 sm:rounded-3xl sm:px-16 after:sm:rounded-3xl md:pt-24 lg:flex lg:gap-x-20 lg:px-24 lg:pt-0">
+            <svg viewBox="0 0 1024 1024" aria-hidden="true"
+                 class="absolute top-1/2 left-1/2 -z-10 size-256 -translate-y-1/2 mask-[radial-gradient(closest-side,white,transparent)] sm:left-full sm:-ml-80 lg:left-1/2 lg:ml-0 lg:-translate-x-1/2 lg:translate-y-0">
+                <circle r="512" cx="512" cy="512" fill="url(#759c1415-0410-454c-8f7c-9a820de03641)" fill-opacity="0.7"/>
+                <defs>
+                    <radialGradient id="759c1415-0410-454c-8f7c-9a820de03641">
+                        <stop stop-color="#7775D6"/>
+                        <stop offset="1" stop-color="#E935C1"/>
+                    </radialGradient>
+                </defs>
+            </svg>
+            <div class="mx-auto max-w-md text-center lg:mx-0 lg:flex-auto lg:py-32 lg:text-left">
+                <h2 class="text-3xl font-semibold tracking-tight text-balance text-white sm:text-4xl">Let's get started!</h2>
+                <p class="mt-6 text-lg/8 text-pretty text-gray-300">Improve your productivity with our app.</p>
+                <div class="mt-10 flex items-center justify-center gap-x-6 lg:justify-start">
+                    <a href="{{route('login')}}"
+                       class="rounded-md bg-gray-700 px-3.5 py-2.5 text-sm font-semibold text-white inset-ring inset-ring-white/5 hover:bg-gray-600 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white">
+                        Login</a>
+                    <a href="{{route('register')}}" class="rounded-md px-3.5 py-2.5 text-sm font-semibold text-gray-400 hover:text-white border border-transparent inset-ring inset-ring-white/5 hover:border-fuchsia-300">
+                        Register
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 </body>
 </html>
